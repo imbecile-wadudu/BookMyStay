dbf33982e1b7ab0f2ad49f3e4375e7fe1608788a
krishna <pantherthekrishna@gmail.com>
2026-04-07 15:12:15 +0530

    Add files via upload

---
+++ BookMyStayApp-feature-UC3/src/BookMyStayApp.java
@@ -0,0 +1,87 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System!");
+        System.out.println("System has started successfully.\n");
+
+        SingleRoom single = new SingleRoom();
+        DoubleRoom doubleRoom = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
+
+        RoomInventory inventory = new RoomInventory();
+
+        System.out.println("Hotel Room Inventory Status\n");
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails(inventory.getRoomAvailability().get("SingleRoom"));
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails(inventory.getRoomAvailability().get("DoubleRoom"));
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails(inventory.getRoomAvailability().get("SuiteRoom"));
+    }
+}
+
+abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails(int available) {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+        System.out.println("Available Rooms: " + available);
+        System.out.println();
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+class RoomInventory {
+    private Map<String, Integer> roomAvailability;
+
+    public RoomInventory() {
+        roomAvailability = new HashMap<>();
+        initializeInventory();
+    }
+
+    private void initializeInventory() {
+        roomAvailability.put("SingleRoom", 5);
+        roomAvailability.put("DoubleRoom", 3);
+        roomAvailability.put("SuiteRoom", 2);
+    }
+
+    public Map<String, Integer> getRoomAvailability() {
+        return roomAvailability;
+    }
+
+    public void updateAvailability(String roomType, int count) {
+        roomAvailability.put(roomType, count);
+    }
+}
